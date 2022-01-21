6f39fda0e4167f4e9de1fa21d13f97381fd4e580
antoniojose2023 <antoniojoseuchoa2023@gmail.com>
2022-01-20 21:47:32 -0300

    Criação do layout do app - tela prinncipal e detalhes

---
+++ app/src/main/java/br/com/antoniojose/simuladorpartidafutebol/DetalhesActivity.kt
@@ -0,0 +1,13 @@
+package br.com.antoniojose.simuladorpartidafutebol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+
+}
